0dd20b9e388d989e22f2736d7cb041e58814e158
Kyota <fj666@mail.ustc.edu.cn>
2025-12-06 18:17:24 +0800

    Convert to static HTML/JS site

--- index.tsx
+++ index.tsx
@@ -1,15 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// This project has been converted to a pure HTML/JS static site.
+// See index.html for the application code.
+console.log("Static Mode");
